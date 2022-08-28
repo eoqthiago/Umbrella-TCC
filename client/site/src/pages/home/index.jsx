ed3e1aec414ed6bed1cd9991c47605e9259a7187
othierrydaora <thierryalefh77@gmail.com>
2022-08-28 01:39:22 -0300

    Cards da landing

--- client/site/src/pages/home/index.jsx
+++ client/site/src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header";
-import { TituloOpen } from "../../styled";
+import { Titulo, SubTitulo } from "../../styled";
+import Card from "../../components/card";
 import "./index.sass";
 
 const Index = () => {
@@ -11,13 +12,19 @@ const Index = () => {
 				<section className="home-banner-inicial">
 					<img src="/assets/images/banner-foguete.png" alt="" />
 					<div>
-						<TituloOpen>BEM-VINDO AO UMBRELLA</TituloOpen>
-						<div>Aqui você pode encontrar comunidades geek!</div>
+						<Titulo>BEM-VINDO AO UMBRELLA</Titulo>
+						<SubTitulo>Aqui você pode encontrar comunidades geek!</SubTitulo>
 					</div>
 				</section>
 				<section className="home-top-comunidades">
 					<div className="home-top-comunidades-titulos">
-						<TituloOpen cor="#3A3A3A">O QUE VOCÊ PROCURA ESTÁ AQUI</TituloOpen>
+						<Titulo cor="#3A3A3A">O QUE VOCÊ PROCURA ESTÁ AQUI</Titulo>
+						<SubTitulo cor="#3A3A3A">Encontre a melhor opção para você!</SubTitulo>
+					</div>
+					<div className="home-top-comunidades-cards">
+						<Card />
+						<Card />
+						<Card />
 					</div>
 				</section>
 			</main>
--- client/site/src/styled.js
+++ client/site/src/styled.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
-const TituloOpen = styled.div`1
-	font-family: OpenSans-semibold !important;
+const Titulo = styled.h1`1
 	font-weigth: 800;
 	font-size: 38px;
 	line-height: 40px;
 	text-transform: uppercase;
 	color: ${(props) => props.cor || "#fff"};
+	font-family: 'OpenSans-bold', sans;
 `;
 
-export { TituloOpen };
+const SubTitulo = styled.h2`
+	margin-top: .5em;
+	font-family: Roboto-regular;
+	font-size: 20px;
+	color: ${(props) => props.cor || "#fff"};
+	`
+
+export { Titulo, SubTitulo };
