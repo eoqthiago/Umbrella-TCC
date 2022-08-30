ca9b0da3a5992c0201ff79dec0aeb7f91a4930e3
othierrydaora <thierryalefh77@gmail.com>
2022-08-30 20:38:04 -0300

    Ajustes no mer e finalização dos scripts ddl

--- server/database/ddl.sql
+++ server/database/ddl.sql
@@ -18,12 +18,11 @@ create table tb_admin (
 create table tb_usuario (
     id_usuario int primary key auto_increment,
     nm_usuario varchar(50) not null,
-    ds_apelido varchar(20) not null,
+    ds_usuario varchar(500) not null,
+    img_usuario varchar(200),
+    img_banner varchar(200),
     ds_email varchar(200) not null,
     ds_senha varchar(64) not null,
-    ds_usuario varchar(500) not null,
-    img_usuario varchar(200) not null,
-    img_banner varchar(200) not null,
     dt_nascimento date not null,
     dt_criacao date default(curdate())
 );
@@ -46,6 +45,7 @@ create table tb_usuario_report (
     id_usuario_reportado int,
     ds_report varchar(500) not null,
     ds_email varchar(250) not null,
+    dt_report date default(curdate()),
     foreign key (id_usuario) references tb_usuario (id_usuario),
     foreign key (id_usuario_reportado) references tb_usuario (id_usuario) on delete cascade
 );
@@ -54,6 +54,7 @@ create table tb_usuario_banido (
     id_usuario_banido int primary key auto_increment,
     id_usuario int,
     ds_motivo varchar(900) not null,
+    dt_banido date default(curdate()),
     foreign key (id_usuario) references tb_usuario(id_usuario)
 );
 
@@ -75,6 +76,7 @@ create table tb_mensagem (
     id_mensagem int primary key auto_increment,
     id_usuario_conversa int,
     ds_mensagem varchar(2500) not null,
+    dt_mensagem datetime default(now()),
     foreign key (id_usuario_conversa) references tb_usuario_conversa (id_usuario_conversa) on delete cascade
 );
 
@@ -82,6 +84,7 @@ create table tb_mensagem_arquivo (
     id_mensagem_arquivo int primary key auto_increment,
     id_usuario_conversa int,
     ds_arquivo varchar(400) not null,
+    dt_arquivo datetime default(now()),
     foreign key (id_usuario_conversa) references tb_usuario_conversa (id_usuario_conversa) on delete cascade
 );
 
@@ -89,9 +92,10 @@ create table tb_mensagem_arquivo (
 create table tb_comunidade (
     id_comunidade int primary key auto_increment,
     id_criador int,
-    nm_comunidade varchar(70) not null,
+    nm_comunidade varchar(50) not null,
     ds_comunidade varchar(700) not null,
-    img_comunidade varchar(200) not null,
+    img_comunidade varchar(200),
+    img_banner varchar(200),
     dt_criacao date default(curdate()),
     foreign key (id_criador) references tb_usuario (id_usuario)
 );
@@ -109,6 +113,7 @@ create table tb_comunidade_mensagem (
     id_mensagem int primary key auto_increment,
     id_usuario_comunidade int,
     ds_mensagem varchar(2500) not null,
+    dt_mensagem datetime default(now()),
     foreign key (id_usuario_comunidade) references tb_usuario_comunidade (id_usuario_comunidade) on delete cascade
 );
 
@@ -116,6 +121,7 @@ create table tb_comunidade_mensagem_arquivo (
     id_mensagem_arquivo int primary key auto_increment,
     id_usuario_comunidade int,
     ds_arquivo varchar(400) not null,
+    dt_arquivo datetime default(now()),
     foreign key (id_usuario_comunidade) references tb_usuario_comunidade (id_usuario_comunidade) on delete cascade
 );
 
@@ -124,6 +130,7 @@ create table tb_comunidade_usuario_banido (
     id_comunidade int,
     id_usuario int,
     ds_motivo varchar(900) not null,
+    dt_banido date default(curdate()),
     foreign key (id_comunidade) references tb_comunidade (id_comunidade) on delete cascade,
     foreign key (id_usuario) references tb_usuario (id_usuario) on delete cascade
 );
@@ -134,6 +141,7 @@ create table tb_comunidade_report (
     id_comunidade int,
     ds_report varchar(500) not null,
     ds_email varchar(250) not null,
+    dt_report date default(curdate()),
     foreign key (id_usuario) references tb_usuario (id_usuario),
     foreign key (id_comunidade) references tb_comunidade (id_comunidade) on delete cascade
 );
@@ -143,6 +151,7 @@ create table tb_feedback (
     id_feedback int primary key auto_increment,
     id_usuario int,
     ds_feedback varchar(1000) not null,
-    lv_estrelas int not null,
+    nr_estrelas int not null,
+    dt_feedback date default(curdate()),
     foreign key (id_usuario) references tb_usuario (id_usuario)
 );
