975359f800e7839345514628b6eaac8d9454b418
othierrydaora <thierryalefh77@gmail.com>
2022-09-28 15:33:48 -0300

    Listagem do menu funcionando

--- client/site/src/components/listas/menu/index.jsx
+++ client/site/src/components/listas/menu/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { BuscarImg } from "../../../api/services";
 import "./index.sass";
 
-const Index = ({ item }) => {
+const Index = ({ item, selecionado }) => {
 	return (
-		<div className="comp-lista-menu">
-			<img src="/assets/images/star-wars.webp" alt="Imagem" />
-            <div>{item.nome}</div>
+		<div className={"comp-lista-menu " + (selecionado && "comp-lista-menu-selecionado")}>
+			<img src={item.imagem ? BuscarImg(item.imagem) : "/assets/images/user.png"} alt="Imagem" />
+			<div>{item.nome}</div>
 		</div>
 	);
 };
