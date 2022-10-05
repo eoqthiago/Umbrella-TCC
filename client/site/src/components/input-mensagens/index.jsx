9ed111cadf4c0e06a526ede60ed4834c930517e1
NauaaN <nauan2006network@gmail.com>
2022-10-05 09:34:02 -0300

    inserir imagens no input(nao acabei)

--- client/site/src/components/input-mensagens/index.jsx
+++ client/site/src/components/input-mensagens/index.jsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 
 export default function Index() {
     const abrirInput = () => document.getElementById("imagem-mensagem").click();
+    const [imagem, setImagem]= useState('');
+    const exibirImagem= () => URL.createObjectURL(imagem);
+    
 
     return (
         <section className="comp-input-mensagem">
-            <img
-                src="/assets/icons/imagem.svg"
-                onClick={() => abrirInput()}
-            />
-            <input type="file" id="imagem-mensagem" />
+            {!imagem && <img src="/assets/icons/imagem.svg" onClick={() => abrirInput()} />}
+            {imagem && <img className="mensagem-imagem" src={exibirImagem()}/>} 
+            <input type="file" id="imagem-mensagem" onChange={e => setImagem(e.target.files[0])}  />
             <input
                 id="mensagem"
                 className="input-texto"
