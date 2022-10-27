d6ba45a9951927427fa02e575487df8e6112e43d
mrlichz <thierryalefh77@gmail.com>
2022-10-27 16:26:49 -0300

    Ajuste após merge

--- client/site/src/api/services.js
+++ client/site/src/api/services.js
@@ -1,6 +1,7 @@
 import localStorage from 'local-storage';
 
 export const userToken = localStorage('user') ? localStorage('user').token : undefined;
+export const adminToken = localStorage('admin') ? localStorage('admin').token : undefined;
 export const baseUrl = 'http://localhost:5050';
 export const socketUrl = 'http://localhost:5051';
 export const BuscarImg = path => `${baseUrl}/${path}`;
