98a6e0ab6978c654111f894ce4674450cf79e863
NauaaN <nauan2006network@gmail.com>
2022-11-13 18:19:53 -0300

    404 page

--- client/site/src/pages/notFound/index.jsx
+++ client/site/src/pages/notFound/index.jsx
@@ -1,7 +1,27 @@
 import "./index.sass";
-
+import { BotaoLiso } from "../../styled";
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/header';
 export default function Index() {
-    <div>
-    </div>
-} ;
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="notfound page">
+            <main>
+                <Header />
+                <div className="pagenotfound">
+                    <div className="notfound-textos">
+                        <h1>404</h1>
+                        <p>Ops! não encontramos para onde você quer navegar:(</p>
+                        <BotaoLiso fonte="4vw" main="#fff" onClick={() => navigate('/')}>Voltar para home</BotaoLiso>
+                    </div>
+
+                </div>
+            </main>
+
+        </div>
+    )
+
+};
 
