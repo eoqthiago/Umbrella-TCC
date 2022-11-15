f3a1eb831f4959f4e27b318d3482d29bc749396b
mrlichz <thierryalefh77@gmail.com>
2022-11-15 19:34:18 -0300

    Procfile e .env

--- client/site/src/api/services.js
+++ client/site/src/api/services.js
@@ -2,7 +2,9 @@ import localStorage from 'local-storage';
 
 export const userToken = localStorage('user') ? localStorage('user').token : undefined;
 export const adminToken = localStorage('admin') ? localStorage('admin').token : undefined;
-export const baseUrl = 'http://localhost:5050';
-export const socketUrl = 'http://localhost:5051';
+export const baseUrl = process.env.REACT_APP_BASE_URL ?? 'http://localhost:5050';
+export const socketUrl = process.env.REACT_APP_SOCKET_URL ?? 'http://localhost:5051';
 export const BuscarImg = path =>
 	typeof path == 'object' ? URL.createObjectURL(path) : `${baseUrl}/${path}`;
+
+console.log(baseUrl, socketUrl)
